8571223fd17109cc31a4e3db4fa26335cc315ca2
Charles <tousignant.chas@gmail.com>
2017-09-17 17:19:04 -0400

    Add Movie and Actor Models

---
+++ src/MovieServer/Models/MovieServer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieServer.Models
+{
+    public class MovieServer
+    {
+        public int Id { get; set; }
+        public string MovieTitle { get; set; }
+        public int MovieRating { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Actors[] Actors { get; set; }
+        public bool Favorite { get; set; }
+    }
+}
