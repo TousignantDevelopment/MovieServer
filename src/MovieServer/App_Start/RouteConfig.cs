00a44b82499f5e965baf8611e130e19970922e3b
Charles <tousignant.chas@gmail.com>
2017-09-17 21:26:18 -0400

    Updated Default Route

--- src/MovieServer/App_Start/RouteConfig.cs
+++ src/MovieServer/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MovieServer
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "MovieServer", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
